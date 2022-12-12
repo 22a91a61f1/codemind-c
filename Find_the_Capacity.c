79ab3faef0da87e6b211fe023665649820e2e43e
22a91a61f1 <119916358+22a91a61f1@users.noreply.github.com>
2022-12-12 11:22:09 +0530

    #include<stdio.h> int main() {     int i,s,t,b,c=1;     int a[3];     for(i=0;i<3;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<3;i++)     {     c=c*a[i];     }     printf("%dKB",c); }

---
+++ Find_the_Capacity.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i,s,t,b,c=1;
+    int a[3];
+    for(i=0;i<3;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<3;i++)
+    {
+    c=c*a[i];
+    }
+    printf("%dKB",c);
+}
